161dd7ef5588688c910473ecea194ed5d8a5b294
IlhamArifinnn <arifinn2208@gmail.com>
2025-05-04 11:03:00 +0700

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500 text-center">
-        Hello world!
-      </h1>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500 text-center">
+        Hello dunia!
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
