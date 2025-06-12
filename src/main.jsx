41fb423244e397d757273fc86d296c5bb6baea3f
IlhamArifinnn <ilha23244ti@student.nurulfikri.ac.id>
2025-06-12 10:30:55 +0700

    uninstall chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
--- src/pages/CreateMovie.jsx
+++ src/pages/CreateMovie.jsx
@@ -1,5 +1,3 @@
-import { Tabs } from "@chakra-ui/react";
-import { LuFolder, LuSquareCheck, LuUser } from "react-icons/lu";
 import styled from "styled-components";
 
 const StyledSection = styled.section`
@@ -13,30 +11,6 @@ function CreateMovie() {
   return (
     <>
       <h2>CreateMovie</h2>
-
-      <StyledSection>
-        <Tabs.Root defaultValue="members">
-          <Tabs.List>
-            <Tabs.Trigger value="members">
-              <LuUser />
-              Members
-            </Tabs.Trigger>
-            <Tabs.Trigger value="projects">
-              <LuFolder />
-              Projects
-            </Tabs.Trigger>
-            <Tabs.Trigger value="tasks">
-              <LuSquareCheck />
-              Settings
-            </Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="members">Manage your team members</Tabs.Content>
-          <Tabs.Content value="projects">Manage your projects</Tabs.Content>
-          <Tabs.Content value="tasks">
-            Manage your tasks for freelancers
-          </Tabs.Content>
-        </Tabs.Root>
-      </StyledSection>
     </>
   );
 }
