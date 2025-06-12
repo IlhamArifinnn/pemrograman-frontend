1ba64866378349361ab4ed793ad622c3fe4b1fdb
IlhamArifinnn <ilha23244ti@student.nurulfikri.ac.id>
2025-06-12 20:03:06 +0700

    build: Consuming API

--- src/pages/CreateMovie.jsx
+++ src/pages/CreateMovie.jsx
@@ -1,16 +1,47 @@
+import { nanoid } from "nanoid";
 import styled from "styled-components";
 
 const StyledSection = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   margin: 2rem;
+
+  .movie__btn {
+    padding: 0.8rem 2rem;
+    border: none;
+    border-radius: 10px;
+    background-color: #901e3e;
+    color: #fff;
+    margin-bottom: 0.5rem;
+    cursor: pointer;
+  }
 `;
 
-function CreateMovie() {
+function CreateMovie({ movies, setMovies }) {
+  function handleClick() {
+    const movie = {
+      id: nanoid(10),
+      title: "Jigsaw Spiral",
+      year: 2021,
+      type: "Movie",
+      poster: "https://picsum.photos/300/400",
+    };
+    // update state movies: setMovies
+    // melakukan spread operator untuk copy and merge array
+    setMovies([...movies, movie]);
+  }
   return (
     <>
-      <h2>CreateMovie</h2>
+      <StyledSection>
+        <div>
+          <h2>CreateMovie</h2>
+        </div>
+        <button className="movie__btn" onClick={handleClick}>
+          Add Movie
+        </button>
+      </StyledSection>
     </>
   );
 }
